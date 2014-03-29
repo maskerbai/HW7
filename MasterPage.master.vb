77a42d4a45753161bbc754778ebab9c1be050838
maskerbai <jintan-bai@uiowa.edu>
2014-03-29 16:35:57 -0500

    add a simple masterpage

---
+++ MasterPage.master.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class MasterPage
+    Inherits System.Web.UI.MasterPage
+
+    Protected Sub Menu1_MenuItemClick(sender As Object, e As MenuEventArgs) Handles Menu1.MenuItemClick
+
+    End Sub
+End Class
+
