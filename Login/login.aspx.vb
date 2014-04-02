46e419f30e47872389981683c902e9539a79c01f
maskerbai <jintan-bai@uiowa.edu>
2014-04-02 15:15:55 -0500

    add role security

---
+++ Login/login.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class Login_login
+    Inherits System.Web.UI.Page
+
+End Class
--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -2,8 +2,6 @@
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
 
-    Protected Sub Menu1_MenuItemClick(sender As Object, e As MenuEventArgs) Handles Menu1.MenuItemClick
-
-    End Sub
+    
 End Class
 
